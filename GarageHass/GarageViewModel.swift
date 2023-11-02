217e3ce7bb67240dcaf459472340942d229a6bce
powskierman <mlapoint99@hotmail.com>
2023-11-01 23:05:34 -0600

    HassFramework is now capturing Home Assistant events.

--- GarageHass/GarageViewModel.swift
+++ GarageHass/GarageViewModel.swift
@@ -14,58 +14,62 @@ class GarageViewModel: ObservableObject {
     @Published var leftDoorClosed: Bool = true
     @Published var rightDoorClosed: Bool = false
     @Published var alarmOff: Bool = true
-    @Published var connectionState: ConnectionState = .disconnected  // Assuming WebSocketConnectionState is an enum or type that describes the connection state
+    @Published var connectionState: ConnectionState = .disconnected
     var connectionStateBinding: Binding<ConnectionState> {
         Binding(
             get: { self.connectionState },
             set: { self.connectionState = $0 }
         )
     }
-    private var websocket: HassWebSocket
-    private var cancellables = Set<AnyCancellable>() // 1. Create a Cancellable set
 
-    
+    private var websocket: HassWebSocket
+    private var cancellables = Set<AnyCancellable>()
+
     init(websocket: HassWebSocket = WebSocketManager.shared.websocket) {
         self.websocket = websocket
         setupWebSocketEvents()
-        
-        // Update connectionState based on websocket's state
-          self.connectionState = websocket.connectionState  // Assuming websocket has a connectionState property
+        self.connectionState = websocket.connectionState
     }
 
-    // Setup WebSocket event handlers
     private func setupWebSocketEvents() {
-          websocket.onEventReceived = { [weak self] event in
-              self?.handleWebSocketEvent(event: event)
-          }
-          
-          // 2. Subscribe to changes in websocket's connectionState
-          websocket.$connectionState
-              .sink { [weak self] newState in
-                  self?.connectionState = newState
-              }
-              .store(in: &cancellables)
-      }
-    
-    // Handle WebSocket events
+        websocket.onEventReceived = { [weak self] event in
+            self?.handleWebSocketEvent(event: event)
+        }
+        websocket.addEventMessageHandler(self)
+        print("I'm at websocket.addEventMessageHandler")
+        websocket.$connectionState
+            .sink { [weak self] newState in
+                self?.connectionState = newState
+            }
+            .store(in: &cancellables)
+    }
+
     private func handleWebSocketEvent(event: String) {
         guard let data = event.data(using: .utf8),
               let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
               let message = jsonObject as? [String: Any],
               let eventType = message["event_type"] as? String, eventType == "state_changed",
               let eventData = message["data"] as? [String: Any],
-              let entityId = eventData["entity_id"] as? String,
-              entityId == "binary_sensor.newgarage_left_door_sensor",
               let newStateData = eventData["new_state"] as? [String: Any],
-              let newState = newStateData["state"] as? String
+              let newState = newStateData["state"] as? String,
+              let entityId = eventData["entity_id"] as? String
         else { return }
         
-        DispatchQueue.main.async {
-            self.leftDoorClosed = (newState == "closed")
+        processStateChange(entityId: entityId, newState: newState)
+    }
+
+    private func processStateChange(entityId: String, newState: String) {
+        if entityId == "binary_sensor.left_door_sensor" {
+            DispatchQueue.main.async {
+                self.leftDoorClosed = (newState == "closed")
+            }
+        } else if entityId == "binary_sensor.right_door_sensor" {
+            DispatchQueue.main.async {
+                self.rightDoorClosed = (newState == "closed")
+            }
         }
     }
-    
-    // Toggle door action
+
     func handleDoorAction(entityId: String) {
         if websocket.isConnected() {
             websocket.setEntityState(entityId: entityId, newState: "toggle")
@@ -80,12 +84,16 @@ class GarageViewModel: ObservableObject {
         }
     }
 }
+
 extension GarageViewModel {
     func establishConnectionIfNeeded() {
+        print("I'm at establishConnectionIfNeeded")
         if !websocket.isConnected() {
             websocket.connect { success in
                 if success {
                     print("Successfully connected!")
+                    self.websocket.subscribeToEvents()
+                    print("I'm at self.websocket.subscribeToEvents")
                 } else {
                     print("Failed to connect to WebSocket.")
                 }
@@ -94,3 +102,12 @@ extension GarageViewModel {
     }
 }
 
+extension GarageViewModel: EventMessageHandler {
+    func handleEventMessage(_ message: HAEventData) {
+        print("I'm at handleEventMessage")
+        guard let newState = message.new_state?.state else {
+            return
+        }
+        processStateChange(entityId: message.entity_id, newState: newState)
+    }
+}
