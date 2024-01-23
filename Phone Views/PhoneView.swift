cd1b8debf5039523f0ace30f9f8b841ff17290c6
powskierman <mlapoint99@hotmail.com>
2024-01-23 09:32:45 -1000

    Fixed the alarm button.

--- Phone Views/PhoneView.swift
+++ Phone Views/PhoneView.swift
@@ -19,7 +19,6 @@ struct PhoneView: View {
                     GarageDoorButton(door: .right)
                         .environmentObject(garageRestManager)
                 }
-                
                 Button(action: {
                     self.showingAlarmConfirmation = true
                 }) {
@@ -30,79 +29,78 @@ struct PhoneView: View {
                 }
                 .frame(width: 250.0, height: 150.0)
                 .padding(EdgeInsets(top: 100, leading: 7, bottom: 0, trailing: 7))
-            }
-            .confirmationDialog("Toggle Alarm", isPresented: $showingAlarmConfirmation) {
-                Button("Confirm", role: .destructive) {
-                    let newState = garageRestManager.alarmOff ? "on" : "off"
-                    let entityIdToToggle = "alarm_control_panel.your_alarm_entity" // replace with your actual entity ID
-                    garageRestManager.handleEntityAction(entityId: entityIdToToggle, newState: newState)
+                
+                .confirmationDialog("Toggle Alarm", isPresented: $showingAlarmConfirmation) {
+                    Button("Confirm", role: .destructive) {
+                        let scriptEntityId = garageRestManager.alarmOff ? "script.toggle_alarm_on" : "script.toggle_alarm_off"
+                        garageRestManager.handleScriptAction(entityId: scriptEntityId)
+                    }
+                }              
+                if let error = garageRestManager.error {
+                    Text("Error: \(error.localizedDescription)")
+                        .foregroundColor(.red)
+                        .padding()
                 }
             }
-            
-            if let error = garageRestManager.error {
-                Text("Error: \(error.localizedDescription)")
-                    .foregroundColor(.red)
-                    .padding()
+            .alert("Connection Error", isPresented: $showingErrorAlert, actions: {
+                Button("Retry") {
+                    // Implement retry logic if necessary
+                }
+            }, message: {
+                if let error = garageRestManager.error {
+                    Text(error.localizedDescription)
+                }
+            })
+            .onChange(of: garageRestManager.hasErrorOccurred) { _, _ in
+                showingErrorAlert = garageRestManager.hasErrorOccurred
+            }
+            .onAppear() {
+                garageRestManager.fetchInitialState()
             }
         }
-        .alert("Connection Error", isPresented: $showingErrorAlert, actions: {
-            Button("Retry") {
-                // Implement retry logic if necessary
+    }
+    
+    struct GarageDoorButton: View {
+        @EnvironmentObject var garageRestManager: GarageRestManager
+        var door: Door
+        
+        var body: some View {
+            Button(action: {
+                // The script entity ID should match what is configured in Home Assistant
+                let scriptEntityId = door == .left ? "script.toggle_left_door" : "script.toggle_right_door"
+                garageRestManager.handleScriptAction(entityId: scriptEntityId)
+            }) {
+                Image(systemName: doorStateImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(doorStateColor)
             }
-        }, message: {
-            if let error = garageRestManager.error {
-                Text(error.localizedDescription)
+            .frame(width: 170.0, height: 170.0)
+        }
+        
+        private var doorStateImage: String {
+            switch door {
+            case .left:
+                return garageRestManager.leftDoorClosed ? "door.garage.closed" : "door.garage.open"
+            case .right:
+                return garageRestManager.rightDoorClosed ? "door.garage.closed" : "door.garage.open"
             }
-        })
-        .onChange(of: garageRestManager.hasErrorOccurred) { _, _ in
-            showingErrorAlert = garageRestManager.hasErrorOccurred
         }
-        .onAppear() {
-            garageRestManager.fetchInitialState()
-        }
-    }
-}
-
-struct GarageDoorButton: View {
-    @EnvironmentObject var garageRestManager: GarageRestManager
-    var door: Door
-
-    var body: some View {
-        Button(action: {
-            // The script entity ID should match what is configured in Home Assistant
-            let scriptEntityId = door == .left ? "switch.toggle_left_door" : "switch.toggle_right_door"
-            garageRestManager.handleEntityAction(entityId: scriptEntityId, newState: "On")
-        }) {
-            Image(systemName: doorStateImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(doorStateColor)
-        }
-        .frame(width: 170.0, height: 170.0)
-    }
-
-    private var doorStateImage: String {
-        switch door {
-        case .left:
-            return garageRestManager.leftDoorClosed ? "door.garage.closed" : "door.garage.open"
-        case .right:
-            return garageRestManager.rightDoorClosed ? "door.garage.closed" : "door.garage.open"
+        
+        private var doorStateColor: Color {
+            switch door {
+            case .left:
+                return garageRestManager.leftDoorClosed ? .teal : .pink
+            case .right:
+                return garageRestManager.rightDoorClosed ? .teal : .pink
+            }
         }
     }
-
-    private var doorStateColor: Color {
-        switch door {
-        case .left:
-            return garageRestManager.leftDoorClosed ? .teal : .pink
-        case .right:
-            return garageRestManager.rightDoorClosed ? .teal : .pink
+    
+    // Provide a preview of the PhoneView.
+    struct PhoneView_Previews: PreviewProvider {
+        static var previews: some View {
+            PhoneView().environmentObject(GarageRestManager.shared)
         }
-    }
-}
-
-// Provide a preview of the PhoneView.
-struct PhoneView_Previews: PreviewProvider {
-    static var previews: some View {
-        PhoneView().environmentObject(GarageRestManager.shared)
     }
 }
